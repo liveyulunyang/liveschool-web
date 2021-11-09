273f077bc17f0e50f53a735daeeb001b4b9d607d
crystal <yulunyangfrontend@gmail.com>
2021-11-09 15:03:00 +0800

    optimaze filter

--- src/components/FilterModal.vue
+++ src/components/FilterModal.vue
@@ -6,6 +6,9 @@
         <select @change="onChange" v-model="progressStatus" class="block appearance-none w-full border border-gray-500 py-3 px-4 pr-8 leading-tight focus:outline-none bg-white focus:border-gray-900" id="role"
           >
           <option value="">狀態</option>
+          <option value="0">審核中</option>
+          <option value="1">通過</option>
+          <option value="2">未通過</option>
         </select>
         <div class=" pointer-events-none absolute inset-y-0 right-0 flex items-center px-1 md:px-2">
           <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
@@ -98,18 +101,10 @@
         <select @change="onChange" v-model="classStatus" class="block appearance-none w-full border border-gray-500 py-3 px-4 pr-8 leading-tight focus:outline-none bg-white focus:border-gray-900" id="role"
           >
           <option value="">課程狀態</option>
-          <option value="all">全部</option>
-          <option value="220">Come to Live</option>
-          <option value="225">自然發音</option>
-          <option value="221">生活英語</option>
-          <option value="223">檢定課程</option>
-          <option value="222">商業英語</option>
-          <option value="224">日文課程</option>
-          <option value="227">互動英語雜誌</option>
-          <option value="289">線上師訓課程</option>
-          <option value="226">雙師課程</option>
-          <option value="320">Ready Go</option>
-          <option value="336">Sound It Out Lite</option>
+          <option value="">課程準備中</option>
+          <option value="" v-if="$store.state.userRole === 'admin' || $store.state.userRole === 'director'">上課中</option>
+          <option value="">已結束</option>
+          <option value="" v-if="$store.state.userRole === 'student' || $store.state.userRole === 'teacher'">前往上課</option>
         </select>
         <div class=" pointer-events-none absolute inset-y-0 right-0 flex items-center px-1 md:px-2">
           <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
